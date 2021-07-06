768e4071186cab2dea3d5ffb1c6ba2ab455124ad
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2021-07-06 18:47:58 +0530

    Add client folder back to git repo

--- index.js
+++ index.js
@@ -20,18 +20,12 @@ app.use(require("./Route/auth"))
 
 const port = process.env.PORT || 5000;
 
-if ( process.env.NODE_ENV == "production"){
-
-    app.use(express.static("client/build"));
-
-    const path = require("path");
-
-    app.get("*", (req, res) => {
-
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-
-    })
-
-}
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
 app.listen(port , console.log(`Server started at ${port}`))

599bd93ab17c4ae4ddf29c2c9b76eee0be170fb3
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2021-07-06 18:56:57 +0530

    make it better

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ app.use(require("./Route/auth"))
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+    app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });

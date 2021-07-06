d018a9c11ec79469170ad9da2d8d0c8198d76e6f
kumarsubhojeet <60726609+kumarsubhojeet@users.noreply.github.com>
2021-07-06 19:02:40 +0530

    make it better

--- index.js
+++ index.js
@@ -20,11 +20,18 @@ app.use(require("./Route/auth"))
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
+if ( process.env.NODE_ENV == "production"){
+
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+
+    app.get("*", (req, res) => {
+
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
+}
 
 app.listen(port , console.log(`Server started at ${port}`))
